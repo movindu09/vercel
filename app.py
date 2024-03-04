b2272c99f485dcf4f2e873766b8630a10f08c8cb
Mohottige Don Isuru Chandika Rathnayaka <125681187+IsuruChandikaRathnayaka@users.noreply.github.com>
2024-03-04 19:53:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -607,5 +607,5 @@ def combine_audio(tone_path, music_path, output_path):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    host = '0.0.0.0'
-    app.run(debug=True,port=10000)
+   
+    app.run(debug=True, host = '0.0.0.0',port=int(os.environ.get(10000)))
